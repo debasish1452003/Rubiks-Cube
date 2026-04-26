c152859113a5eb4b0c0367f4c9381897ad93eedb
debasish1452003 <debasishrana1452003@gmail.com>
2026-04-26 21:41:12 +0530

    IDA star file implemented.

---
+++ PatternDatabases/CornerPatternDatabase.h
@@ -0,0 +1,20 @@
+#ifndef RUBIKS_CUBE_SOLVER_CORNERPATTERNDATABASE_H
+#define RUBIKS_CUBE_SOLVER_CORNERPATTERNDATABASE_H
+
+#include "../Model/RubiksCube.h"
+#include "PatternDatabase.h"
+#include "PermutationIndexer.h"
+using namespace std;
+
+class CornerPatternDatabase : public PatternDatabase
+{
+    typedef RubiksCube::FACE f;
+    PermutationIndexer<8> permIndexer;
+
+public:
+    CornerPatternDatabase();
+    CornerPatternDatabase(uint8_t init_val);
+    uint32_t getDatabaseIndex(const RubiksCube &cube) const;
+}
+
+#endif
